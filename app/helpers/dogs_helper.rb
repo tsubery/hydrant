95826ca491665fe4863d261ac16abcc326a7628b
Gal Tsubery <gal.tsubery@gmail.com>
2018-05-21 21:52:24 -0500

    Dog photos upload

--- app/helpers/dogs_helper.rb
+++ app/helpers/dogs_helper.rb
@@ -1,2 +1,9 @@
 module DogsHelper
+  def profile_photo(dog)
+    if dog.profile.attachment
+      image_tag(dog.profile.variant(resize: "400x400"))
+    else
+      image_tag  "dog_icon.jpg", width: 100, height: 100
+    end
+  end
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,4 @@
 class Dog < ApplicationRecord
+  has_one_attached :profile
+  has_many_attached :photos
 end
