2f57084f0b336c2efd19edc13f69d37989d696e1
Gal Tsubery <gal.tsubery@gmail.com>
2018-05-22 17:05:43 -0500

    Add attach test

--- test/models/dog_test.rb
+++ test/models/dog_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class DogTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "attach 1000 photos" do
+    dog_photo = StringIO.new(File.read("test/dog.jpg"))
+    rover = Dog.create!(name: "Rover", owner: "owner")
+
+    (1..200).each do |i|
+      print '.' if (i % 20).zero?
+
+      rover.photos.attach(
+        io: dog_photo.tap(&:rewind),
+        filename: "#{i}.jpg",
+        content_type: "image/jpg"
+      )
+    end
+  end
 end
